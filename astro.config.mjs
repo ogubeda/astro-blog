25b86ea2c4a1c42e7472768bdec3765970a27b6b
Oscar Gandia <og.ubeda@gmail.com>
2023-12-18 12:59:52 +0100

    Desarrollo tags y rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://657c2f6ab617380009edcc4c--adorable-douhua-b07f2e.netlify.app/'
+});
