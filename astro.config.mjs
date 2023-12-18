74dbc1729734c025d5685f716446f0b3452913b0
Oscar Gandia <og.ubeda@gmail.com>
2023-12-18 13:08:06 +0100

    Componentes externos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://657c2f6ab617380009edcc4c--adorable-douhua-b07f2e.netlify.app/'
+  site: 'https://657c2f6ab617380009edcc4c--adorable-douhua-b07f2e.netlify.app/',
+  integrations: [react()]
 });
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+export default function Greeting({messages}) {
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))]
+
+  const [greeting, setGreeting] = useState(randomMessage())
+
+  return (
+    <div>
+      <h3>{greeting} ¡Gracias por tu visita!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>
+        Nuevo saludo
+      </button>
+    </div>
+  );
+}
